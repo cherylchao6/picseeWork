7a558fc53c6885527d15b240f4cd9f9d5540f04a
cherylchao6 <nmpyt21@gmail.com>
2021-07-23 13:14:01 +0800

    without user queryparameter

--- app.js
+++ app.js
@@ -49,15 +49,37 @@ function getCache (key) {
 app.get("/", verifyUser, async (req,res) => {
   let {user} = req.query;
   if (!user) {
-    console.log('26')
-    let data = {
-      count: 1,
-      time: new Date().getTime()
-    };
     let clientip = req.connection.remoteAddress;
-    client.setex(clientip, 600, JSON.stringify(data));
-    res.sendStatus(401);
-    return 
+    let value = await getCache(clientip);
+    if (value === null) {
+      let data = {
+        count: 1,
+        time: new Date().getTime()
+      };
+      client.setex(clientip, 600, JSON.stringify(data));
+      let message = {
+        message: `目前造訪1次`
+      }
+      res.status(200).send(message)
+      return
+    }
+    let parsedValue = JSON.parse(value);
+    if (parsedValue.count >= 5) {
+      let time = JSON.parse(value)['time']
+      let expiredTime = time + 600000;
+      var clock = new Date(expiredTime);
+      let message = {
+          message: `請於${clock}再造訪`
+      }
+      res.status(429).send(message);
+    } else {
+      parsedValue.count += 1;
+      client.set(user,JSON.stringify(parsedValue));
+      let message = {
+        message: `目前造訪${parsedValue.count}次`
+      }
+      res.status(200).send(message);
+    }
   } 
   if (user !== 'admin') {
     let limitation = req.limitation
@@ -92,7 +114,6 @@ app.get("/", verifyUser, async (req,res) => {
       res.status(200).send(message)
     }
   } else {
-    console.log('72')
     return res.sendStatus(200)
   }
 })
